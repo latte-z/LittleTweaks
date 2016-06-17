681981b39634ff70c72dfa0715ee780c3443f9b6
Ryan Shen <wei.sheng0107@gmail.com>
2016-06-17 19:29:20 +0800

    Remove v7-support & Startup activity view

--- NetworkType/app/src/main/java/com/lattez/networktype/MainActivity.java
+++ NetworkType/app/src/main/java/com/lattez/networktype/MainActivity.java
@@ -1,15 +1,15 @@
 package com.lattez.networktype;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by weish on 2016/5/12.
+ * Created by ryan on 2016/5/12.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
             Intent changeNetwork = new Intent();
             changeNetwork.setClassName("com.android.settings", "com.android.settings.RadioInfo");
             startActivity(changeNetwork);
-            finish();
+            this.finish();
         } catch (java.lang.Exception e) {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setMessage("您的手机不支持打开网络设置，请卸载软件");
